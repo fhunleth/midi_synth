63951f434c43f897ca1b93d88f1f95d9d564f190
Frank Hunleth <fhunleth@troodon-software.com>
2020-08-02 19:27:28 -0400

    ex_docs: bump and add update config

--- mix.exs
+++ mix.exs
@@ -2,24 +2,28 @@ defmodule MidiSynth.MixProject do
   use Mix.Project
 
   @version "0.2.0"
-
-  @description "MIDI synthesizer for Elixir"
+  @source_url "https://github.com/fhunleth/midi_synth"
 
   def project do
     [
       app: :midi_synth,
       version: @version,
       elixir: "~> 1.6",
-      description: @description,
       package: package(),
       compilers: [:elixir_make | Mix.compilers()],
       aliases: [format: ["format", &format_c/1]],
       make_clean: ["clean"],
-      docs: [extras: ["README.md"], main: "readme"],
+      docs: docs(),
+      description: description(),
       start_permanent: Mix.env() == :prod,
       build_embedded: true,
       dialyzer: dialyzer(),
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: %{
+        docs: :docs,
+        "hex.publish": :docs,
+        "hex.build": :docs
+      }
     ]
   end
 
@@ -30,7 +34,11 @@ defmodule MidiSynth.MixProject do
     ]
   end
 
-  defp package() do
+  defp description do
+    "MIDI synthesizer for Elixir"
+  end
+
+  defp package do
     [
       files: [
         "lib",
@@ -41,14 +49,14 @@ defmodule MidiSynth.MixProject do
         "Makefile"
       ],
       licenses: ["Apache-2.0"],
-      links: %{"GitHub" => "https://github.com/fhunleth/midi_synth"}
+      links: %{"GitHub" => @source_url}
     ]
   end
 
   defp deps do
     [
       {:elixir_make, "~> 0.5", runtime: false},
-      {:ex_doc, "~> 0.11", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.22", only: :docs, runtime: false},
       {:dialyxir, "1.0.0", only: :dev, runtime: false}
     ]
   end
@@ -56,6 +64,16 @@ defmodule MidiSynth.MixProject do
   defp dialyzer() do
     [
       flags: [:race_conditions, :unmatched_returns, :error_handling, :underspecs]
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md", "CHANGELOG.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
     ]
   end
 
