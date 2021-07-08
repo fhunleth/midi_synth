0fe9f80eaddfcee72523bef27f7b2e7cf1e26e62
Frank Hunleth <fhunleth@troodon-software.com>
2021-07-08 19:53:01 -0400

    Bump all deps

--- mix.exs
+++ mix.exs
@@ -63,7 +63,7 @@ defmodule MIDISynth.MixProject do
     [
       {:elixir_make, "~> 0.6", runtime: false},
       {:ex_doc, "~> 0.22", only: :docs, runtime: false},
-      {:dialyxir, "1.0.0", only: :dev, runtime: false},
+      {:dialyxir, "1.1.0", only: :dev, runtime: false},
       {:credo, "~> 1.2", only: :test, runtime: false}
     ]
   end
